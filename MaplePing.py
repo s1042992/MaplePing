57e98d6f5231ab559da4b725db46008693a97500
s1042992 <40089665+s1042992@users.noreply.github.com>
2021-09-04 22:52:02 +0800

    change to numpy

--- MaplePing.py
+++ MaplePing.py
@@ -1,5 +1,10 @@
-import os 
+import os
+import re
+import time
+from queue import Queue 
 from tcp_latency import measure_latency
+from threading import Thread
+import numpy as np
 
 print("******************")
 print("伺服器         數字")
@@ -13,9 +18,10 @@ print("殺人鯨:   	 6")
 print("Reboot:		45")
 print()
 print("******************")
-print("上面是每個伺服器對應的數字，請輸入數字來查看伺服器延遲狀況:")
+print("上面是每個伺服器對應的數字，請輸入數字來查看本機與伺服器之間延遲狀況:")
 print("(Here is the list of world, please enter a world number)")
 w = {0, 1, 2, 3, 4, 6, 45}
+q = Queue()
 while True:
 	world = input()
 	try:
@@ -44,47 +50,58 @@ else:
 	mall_ip = "202.80.104." + str(40 + world)
 	auction_ip = "202.80.104." + str(40 + world)
 	
-rtt = []
-def ping(ch):
+channel_rtt = np.array(range(40))
+
+def ping(ch): #ping task
 	if ch % 2 == 0:
 		ch_port = 8585
 	else:
 		ch_port = 8686
+		
 	if ch > 30:
-		ip = "202.80.104." + str(((ch - 30) // 2) + 154) # only happens in world 1 
+		ip = "202.80.104." + str(((ch - 30) // 2) + 154) # only happens in world 0 
 	else:
-		ip = "202.80.104." + str((ch//2) + init)
+		ip = "202.80.104." + str((ch // 2) + init)
+		
 	result = measure_latency(host = ip, port = ch_port)
 	if result is None:
-		result = 9999999
-	print("CH.",ch+1,"= ", round(result[0],3), "ms")
-	return round(result[0],3)
+		result = 999999
+	print("CH.", ch+1,"= ", round(result[0],3), "ms")
+	channel_rtt[ch] = round(result[0],3)
 	
-	
+
 if world == 0: #World 0 has 40 channels
 	try:
 		for i in range(40):
-			rtt.append(ping(i))
+			ping(i)
+			
 	except:
 		pass
 else:
 	try:
 		for i in range(30):
-			rtt.append(ping(i))
+			ping(i)
 	except:
 		pass
-		
+
+
 print("副本: ", round(measure_latency(host = dungeon_ip, port = 8686)[0],3), "ms")
 print("商城: ", round(measure_latency(host = mall_ip, port = 8686)[0],3), "ms")
 
 if world != 45: #World Reboot has no Auction system
 	print("拍賣: ", round(measure_latency(host = auction_ip, port = 8787)[0],3), "ms")
-	  
-max_value = max(rtt)
-min_value = min(rtt)
+
+channel_rtt = channel_rtt.tolist()
+if world != 0:	 
+	max_value = max(channel_rtt[0:30])
+	min_value = min(channel_rtt[0:30])
+else:
+	max_value = max(channel_rtt)
+	min_value = min(channel_rtt)
+
 print()
-print("Maximum delay CH.", rtt.index(max_value)+1, ",RTT = ", max_value, "ms")
-print("Minimal delay CH.", rtt.index(min_value)+1, ",RTT = ", min_value, "ms")
-print("建議去 CH.",rtt.index(min_value)+1)
+print("Maximum delay CH.", channel_rtt.index(max_value)+1, ",RTT = ", max_value, "ms")
+print("Minimal delay CH.", channel_rtt.index(min_value)+1, ",RTT = ", min_value, "ms")
+print("建議去 CH.",channel_rtt.index(min_value)+1)
 print()
 os.system("pause")
