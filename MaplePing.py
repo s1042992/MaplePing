dba397a3cb439b914421ad545e2753fe42848ffd
s1042992 <40089665+s1042992@users.noreply.github.com>
2021-10-12 22:18:43 +0800

    change the order of execution
    change the order of execution and add a loop at the begining, so that the user can keep testing the ping

--- MaplePing.py
+++ MaplePing.py
@@ -35,67 +35,73 @@ if __name__ == '__main__':
 	print("Reboot:		45")
 	print()
 	print("******************")
-	print("上面是每個伺服器對應的編號，請輸入編號來查看本機與伺服器之間延遲狀況:")
-	print("(Here is the list of world, please enter a world number)")
+	
 	w = {0, 1, 2, 3, 4, 6, 45}
 	while True:
-		world = input()
-		try:
-			world = int(world)
-			if world in w:
-				print("Please wait for a while...")
-				break
-			else:
+		print()
+		print("上面是每個伺服器對應的編號，請輸入編號來查看本機與伺服器之間延遲狀況:")
+		print("(Please enter a world number based on the list above.)")
+		channel_rtt = np.array(range(40), np.float)
+		while True:
+			world = input()
+			try:
+				world = int(world)
+				if world in w:
+					print("Please wait for a while...")
+					break
+				else:
+					print("Please re-enter a legal world number:")
+					continue
+			except:
 				print("Please re-enter a legal world number:")
 				continue
-		except:
-			print("Please re-enter a legal world number:")
-			continue
+			
+		if world == 6: # there's no world 5
+			world = world - 1
+
+		if world == 45: #World Reboot is very special...
+			init = 164
+			dungeon_ip = "202.80.104.39"
+			mall_ip = "202.80.104.47"
+		else:
+			init = 64 + world * 15
+			dungeon_ip = "202.80.104." + str(32 + world)
+			mall_ip = "202.80.104." + str(40 + world)
+			auction_ip = "202.80.104." + str(40 + world)
+			
+		if world == 0: #World 0 has 40 channels
+			try:
+				for i in range(40):
+					ping_job(i)
+					
+			except:
+				pass
+		else:
+			try:
+				for i in range(30):
+					ping_job(i)
+			except:
+				pass
+			
 		
-	if world == 6: # there's no world 5
-		world = world - 1
 
-	if world == 45: #World Reboot is very special...
-		init = 164
-		dungeon_ip = "202.80.104.39"
-		mall_ip = "202.80.104.47"
-	else:
-		init = 64 + world * 15
-		dungeon_ip = "202.80.104." + str(32 + world)
-		mall_ip = "202.80.104." + str(40 + world)
-		auction_ip = "202.80.104." + str(40 + world)
-		
-	if world == 0: #World 0 has 40 channels
-		try:
-			for i in range(40):
-				ping_job(i)
-				
-		except:
-			pass
-	else:
-		try:
-			for i in range(30):
-				ping_job(i)
-		except:
-			pass
-		
-	print("副本: ", round(measure_latency(dungeon_ip, 8686)[0],3), "ms")
-	print("商城: ", round(measure_latency(mall_ip, 8686)[0],3), "ms")
+		channel_rtt = channel_rtt.tolist()
+		if world != 0:	 
+			max_value = max(channel_rtt[0:30])
+			min_value = min(channel_rtt[0:30])
+		else:
+			max_value = max(channel_rtt)
+			min_value = min(channel_rtt)
 
-	if world != 45: #World Reboot has no Auction system
-		print("拍賣: ", round(measure_latency(auction_ip, 8787)[0],3), "ms")
+		print()
+		print("Maximum delay CH.", channel_rtt.index(max_value)+1, ",RTT = ", max_value, "ms")
+		print("Minimal delay CH.", channel_rtt.index(min_value)+1, ",RTT = ", min_value, "ms")
+		print("建議去 CH.",channel_rtt.index(min_value)+1)
+		print()
+		print("副本: ", round(measure_latency(dungeon_ip, 8686)[0],3), "ms")
+		print("商城: ", round(measure_latency(mall_ip, 8686)[0],3), "ms")
 
-	channel_rtt = channel_rtt.tolist()
-	if world != 0:	 
-		max_value = max(channel_rtt[0:30])
-		min_value = min(channel_rtt[0:30])
-	else:
-		max_value = max(channel_rtt)
-		min_value = min(channel_rtt)
-
-	print()
-	print("Maximum delay CH.", channel_rtt.index(max_value)+1, ",RTT = ", max_value, "ms")
-	print("Minimal delay CH.", channel_rtt.index(min_value)+1, ",RTT = ", min_value, "ms")
-	print("建議去 CH.",channel_rtt.index(min_value)+1)
-	print()
+		if world != 45: #World Reboot has no Auction system
+			print("拍賣: ", round(measure_latency(auction_ip, 8787)[0],3), "ms")
+			
 	os.system("pause")
